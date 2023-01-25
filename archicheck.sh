a7be37741c845a9592ab269987a1c81cbf014026
kraken <70107358+Kraken908@users.noreply.github.com>
2023-01-25 11:09:55 -0700

    Create archicheck.sh

---
+++ archicheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                     
+
+ARCHITECTURE=`uname -m`                                                         
+if [ $ARCHITECTURE = "i686" ]                                                   
+then                                                                            
+        echo "32 Bit Operating System Detected"                                 
+fi                                                                              
+if [ $ARCHITECTURE = "x86_64" ]                                                 
+then                                                                            
+        echo "64 Bit Operating System Detected"                                 
+fi
